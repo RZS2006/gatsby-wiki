7eada3375f3bfa6b4cdc776194d555eb07b515bf
Raiyan Shamim <razash139@gmail.com>
2021-06-10 17:02:39 +0300

    fetch data template

--- src/templates/wiki-article.js
+++ src/templates/wiki-article.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 
-const WikiArticlePage = () => {
+const WikiArticlePage = ({ data }) => {
+	const article = data.markdownRemark;
+
 	return (
 		<Layout>
 			<div className="container">
-				<h1>Article Title</h1>
-				<p>Article Description</p>
-				<div dangerouslySetInnerHTML={{ __html: 'Article HTML' }}></div>
+				<h1>{article.frontmatter.title}</h1>
+				<p>{article.frontmatter.description}</p>
+				<div dangerouslySetInnerHTML={{ __html: article.html }}></div>
 			</div>
 		</Layout>
 	);
 };
 
 export default WikiArticlePage;
+
+export const query = graphql`
+	query WikiArticleById($id: String!) {
+		markdownRemark(id: { eq: $id }) {
+			frontmatter {
+				title
+				description
+				slug
+				category
+				published
+			}
+			html
+			id
+		}
+	}
+`;
