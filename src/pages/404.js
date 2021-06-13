ebafb4f8de7a81978606b945198def972a7ff5f0
Raiyan Shamim <razash139@gmail.com>
2021-06-13 15:19:18 +0300

    add styles to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
+import * as styles from '../styles/404.module.css';
 
 const NotFoundPage = () => {
 	return (
@@ -10,7 +11,9 @@ const NotFoundPage = () => {
 			<div className="container">
 				<h1>404</h1>
 				<p>Page not found</p>
-				<Link to="/">Go back to the homepage</Link>
+				<Link to="/" className={styles.link}>
+					Go back to the homepage
+				</Link>
 			</div>
 		</Layout>
 	);
